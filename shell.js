1491927e6b4baefdde966f70556ece862f050c3d
Jesse Tane <jesse.tane@gmail.com>
2012-08-02 03:05:01 -0400

    fix

--- shell.js
+++ shell.js
@@ -7,11 +7,7 @@ var exec = require('child_process').exec;
 module.exports = function (cmd, cb) {
   var data = "";
   var proc = exec(cmd);
-  proc.stdout.on("data", recorder);
-  proc.stderr.on("data", recorder);
+  proc.stdout.on("data", function (d) { data += d });
+  proc.stderr.on("data", function (d) { data += d });
   proc.on("exit", function () { cb(data.slice(0,-1)) });
-}
-
-function recorder (d) { 
-  data += d
 }
