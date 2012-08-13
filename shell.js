cdb3318fb4cbc532c5b852bf6c86dce8dc2c5b9b
Jesse Tane <jesse.tane@gmail.com>
2012-08-13 12:04:55 -0400

    made callback optional

--- shell.js
+++ shell.js
@@ -7,7 +7,9 @@ var exec = require('child_process').exec;
 module.exports = function (cmd, cb) {
   var data = "";
   var proc = exec(cmd, function (err) {
-    cb(err, data.slice(0,-1));
+    if (cb) {
+      cb(err, data.slice(0,-1));
+    }
   });
   proc.stdout.on("data", function (d) { data += d });
   proc.stderr.on("data", function (d) { data += d });
