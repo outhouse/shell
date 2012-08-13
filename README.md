c0db3bc412d56389002472b99c5c96027c4ad6d5
Jesse Tane <jesse.tane@gmail.com>
2012-08-13 00:58:57 -0400

    added error handling

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ An async shell command executor for nodejs
 Usage:
 
 	shell = require("shell");
-	shell("ls -alh", function (data) {
-		console.log(data);
+	shell("ls -alh", function (err, data) {
+		console.log(err, data);
 	});
--- shell.js
+++ shell.js
@@ -6,8 +6,9 @@ var exec = require('child_process').exec;
 
 module.exports = function (cmd, cb) {
   var data = "";
-  var proc = exec(cmd);
+  var proc = exec(cmd, function (err) {
+    cb(err, data.slice(0,-1));
+  });
   proc.stdout.on("data", function (d) { data += d });
   proc.stderr.on("data", function (d) { data += d });
-  proc.on("exit", function () { cb(data.slice(0,-1)) });
 }
